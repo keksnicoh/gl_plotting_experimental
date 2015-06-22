c7ab45d589515a833ed2184c2213d3182a6d7b62
Jesse Hinrichsen <Jesse Hinrichsen>
2015-06-22 19:47:18 +0200

    better for feigenbaumkonstante

--- pyplot_examples/6.2.py
+++ pyplot_examples/6.2.py
@@ -2,13 +2,22 @@
 # Example Plot from Script
 import matplotlib.pyplot as plt
 import math
+import time
+
+from pymongo import MongoClient
 
 import numpy as np
+import sys, os, getopt
 
 class chaos(object):
 	"""docstring for chaos"""
-	def __init__(self):
+	def __init__(self, persistToDb=False):
 		super(chaos, self).__init__()
+		self.client = None
+		if persistToDb:
+			self.client = MongoClient('localhost', 27017)
+			self.db = self.client['feigenbaum']
+			#self.logCollection = self.db['logCollection']
 
 
 	def log_dr(self, x, a):
@@ -191,27 +200,103 @@ class chaos(object):
 			r += d_r
 
 	# Trying to find periodenverdopplung by searching specified ranges
+	# converging agains verdopplung by reducing range ba half
+	# Calculated values:
+	# 4: 	3.448975032407762
+	# 8: 	3.5440064530845543
+	# 16: 	3.5643923499537324
+	# 32: 	3.5687576157816836
+	# 64: 	3.569691168618376
+	# 128: 	3.5698910781960196
+	#
+	# Closest Feigenbaumkonstante: 4.67002416 (With data up to 32)
+	# Next Feigenbaumkonstante: 4.67404022 (With data up to 64) -- Took around 10 Minutes
 	def findVerdopplung(self):
-		r = 3.4
+		r = 3.3
+		end = 3.57
 		data = []
-		for periods in [2] * 100:
+		formerPeriod = 2
+		for periods in [2]*4 + [4]*4 + [8]*4 + [16]*4 + [32]*4 + [64]*4:
 			subdata = []
-			d_r = (3.57 - r) / 4000.
-			while r < 3.57:
-				(x,y) = self.iterate(self.logAbbildung, 0.5, (r,), 5000)
+			if formerPeriod != periods:
+				end = 3.57
+			formerPeriod = periods
+			d_r = (end - r) / 1000.
+			while r < end:
+				y = self.fastIterate(self.logAbbildung, 0.5, (r,), int(0.1/d_r))
 
-				if np.isclose([y[-1]], [y[-(periods + 1)]], rtol=1e-20, atol=1e-20) and not np.isclose([y[-1]], [y[-periods]], rtol=1e-20, atol=1e-20):
-					subdata.append((r, periods))
-					#print r, periods
+				if np.isclose([y[-1]], [y[-(periods + 1)]], rtol=d_r, atol=d_r) and not np.isclose([y[-1]], [y[-periods]], rtol=d_r, atol=d_r):
+					subdata.append((r, periods*2))
+				elif subdata:
+					end = r + d_r*40
+					print "End %.15f" % end
+					break
 
 				r += d_r
-			data.append(subdata[1])
-			r = subdata[-1][0]
+			data.append(subdata[-1])
+			r = subdata[len(subdata) / 2][0]
 
-			#if len(data) > 2:
-				#print "Feigenbaum: %.8f based on %s" % ((data[-2][0] - data[-3][0]) / (data[-1][0] - data[-2][0]), data)
+			if len(data) > 12:
+				print "Feigenbaum: %.8f based on %s" % ((data[-5][0] - data[-9][0]) / (data[-1][0] - data[-5][0]), data)
 			print data[-1]
-		#print data
+
+	# Better
+	# Results in mongodb or down below
+	def findVerdopplungOptimzed(self, abbildung, periodCount):
+		r = 3.3
+		end = 3.57
+		data = []
+		formerPeriod = 2
+		periodIteration = 100
+		for periods in [periodCount]*periodIteration:
+			start_time = time.time()
+			subdata = []
+			if formerPeriod != periods:
+				end = 3.57
+			formerPeriod = periods
+			d_r = (end - r) / 100.
+			while r < end:
+				iterations = int(0.1/d_r)
+				y = self.fastIterate(abbildung, 0.5, (r,), iterations)
+
+				if np.isclose([y[-1]], [y[-(periods + 1)]], rtol=d_r, atol=d_r) and not np.isclose([y[-1]], [y[-periods]], rtol=d_r, atol=d_r):
+					d = (r, periods*2, time.time() - start_time)
+					subdata.append(d)
+				elif subdata:
+					end = r + d_r*40
+					print "End %.15f" % end
+					break
+
+				r += d_r
+			data.append(subdata[-1])
+			if self.db:
+				post = {
+					"r" : subdata[-1][0],
+					"periods": subdata[-1][1],
+					"calculationTime": subdata[-1][2],
+					"d_r": d_r,
+					"iterationsPerR": iterations,
+					"end": end
+				}
+				posts = self.db.posts
+				post_id = posts.insert_one(post).inserted_id
+				print "Insterted Data with id: %s" % post_id
+
+				if subdata[-1][2] > 60*10:
+					print "Calculation time exceeded 10 minutes. Aborting now"
+					break
+
+			r = subdata[len(subdata) / 2][0]
+			del subdata
+
+			#if len(data) > periodIteration*3:
+			#	print "Feigenbaum: %.8f based on %s" % ((data[-(1+periodIteration)][0] - data[-(1+periodIteration*2)][0]) / (data[-1][0] - data[-(1+periodIteration)][0]), data)
+			print data[-1]
+
+	def listData(self):
+		posts = self.db.posts
+		for post in posts.find():
+			print post
 
 	def estimatePeriodenverdopplung(self):
 		r = 3.4
@@ -245,11 +330,41 @@ class chaos(object):
 
 		#self.plotScatter([(x,y)], (990,1000), (0,1))
 
+
+# Running the Feigenbaum calculation on a remote server for different periods rtunrs following Data
+# LogAbbildung:
+# 4: 3.449487092418365, time for loop: 36.21665811538696
+# 8: 3.5440894317360905, time for loop: 26.39645504951477
+
 if __name__ == '__main__':
+	helpText = '6.2.py -p [look for how many periods] -d'
+	periods = None
+	data = False
+
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"p:d",["pfile="])
+	except getopt.GetoptError:
+		print helpText
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print helpText
+			sys.exit(1)
+		elif opt in ("-p", "--pfile"):
+			periods = arg
+		elif opt in ("-d"):
+			periods = None
+			data = True
 	#dreieckPlot()
-	app = chaos()
+	app = chaos(True)
 	#app.optimizedPeriodenverdopplung((1,4), app.logAbbildung, 0.3, 20000)
-	app.findVerdopplung()
+	if periods and int(periods) >= 2 and int(periods) <= 100000:
+		print "Find for %s periods" % periods
+		app.findVerdopplungOptimzed(app.logAbbildung, int(periods))
+
+	if data:
+		print "Displaying Data"
+		app.listData()
 	#app.dreieckPlot(0.8, 0.98)
 	#app.bifrac(app.logAbbildung, (1, 4), 0.002, 500)
 	#app.bifrac(app.sinAbblidung, (1,4), 0.002, 500)
