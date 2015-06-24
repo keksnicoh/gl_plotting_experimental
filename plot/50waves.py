0b437a57532d5473173af77b8cc13081c3e5f2ff
Nicolas 'keksnicoh' Heimann <keksnicoh@heimann.hamburg>
2015-06-24 12:52:44 +0200

    added 50waves to plot

---
+++ plot/50waves.py
@@ -0,0 +1,19 @@
+from app.plot2d import plotter_app
+from mygl.objects.plot2d import cartesian_domain, x_axis_domain
+
+KERNEL = """
+float c;
+float tx;
+float ty;
+vec4 f(vec4 x) {
+    c = 0.0;
+    for (i=0;i < 50;i+=1) {
+        tx = x.x + 0.5 -0.02*i;
+        ty = x.y - 0.1 + 0.001*i;
+        c += sin(50*sqrt(tx*tx + ty*ty)-5*time);
+    }
+    return vec4(x.xy, c / 7, vertex_position.w);
+}
+"""
+
+plotter_app(KERNEL, cartesian_domain(1000, 3.0, 3.0))
