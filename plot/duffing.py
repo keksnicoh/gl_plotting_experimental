96bfe6a818a5e2e48eeded95390356a3171fe1be
Jesse Hinrichsen <Jesse Hinrichsen>
2015-06-30 21:57:50 +0200

    added duffing oszillation kernel

--- plot/duffing.py
+++ plot/duffing.py
@@ -31,11 +31,26 @@ PHASE_KERNEL = """#pragma OPENCL EXTENSION cl_khr_fp64 : enable
 """
 Kernel to calculate duffing equation and writes (t, x) data to given GLBuffer
 """
-OSZILATION_KERNEL = """
+OSZILATION_KERNEL = """#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+    __kernel void f(float2 awp, int iterations, int time, float lambda, float beta, float omega, float epsilon, __global float *result)
+    {
 
+        float h = time / convert_float(iterations);
+        float theta = 0;
+        result[0] = 0.0;
+        result[1] = awp.x;
+        float y = awp.y;
+        for(int i=2; i < iterations; i += 2) {
+            theta = i / (2.0 * iterations) * time * omega;
+            result[i] = (i / convert_float(iterations)) * time;
+            result[i+1] = result[i-1] + h * y;
+            y = y + h * (epsilon * cos(theta) - lambda * y - beta * result[i-1] * result[i-1] * result[i-1]);
+
+        }
+    }
 """
 
 window = PlotterWindow(axis=(10.0,10.0), origin=(5.0,5.0))
-duffing_domain = domain.DuffingDomain(PHASE_KERNEL, 50000, 60, 0.08, 1, 1, 1, (3,4))
+duffing_domain = domain.DuffingDomain(OSZILATION_KERNEL, 50000, 60, 0.08, 1, 1, 1, (3,4))
 window.plotter.add_graph('duffing', graph.Discrete2d(duffing_domain))
 window.run()
