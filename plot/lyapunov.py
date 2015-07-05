b368c93c4dcabae3aa194fe443abf506e03bd5de
Jesse Hinrichsen <Jesse Hinrichsen>
2015-07-05 16:34:10 +0200

    finally working

--- plot/lyapunov.py
+++ plot/lyapunov.py
@@ -9,20 +9,22 @@ from plotting import graph, domain
 
 SIMPLE = """
 float g(float r, float x) {
-    return log(abs(r * cos(x)));
+    float v = abs(r * cos(x));
+    return v;
 }
 
 float h(float r, float x) {
-    return log(abs(r - 2*r*x));
+    float v = abs(r - 2*r*x);
+    return v;
 }
 
 vec4 f(vec4 x) {
-    float y0 = x.y;// = g(x.x, x.y);
-    float summe = 0;
+    float y0 = 10;// = g(x.x, x.y);
+    float summe = y0;
     float n = 1000;
     for (int i = 1; i < n - 1; i+=1) {
         y0 = h(x.x, y0);
-        summe += y0;
+        summe += log(y0);
     }
     return vec4(x.x, summe/n, 0, 0.3);
 }
@@ -30,7 +32,8 @@ vec4 f(vec4 x) {
 
 
 window = PlotterWindow(axis=(3.0,7.0), origin=(-1.0,5.0))
-domain = domain.Cartesian(100, min_y=0.01)
-domain.transformation_matrix = domain.fixed_y_transformation
+#domain = domain.Cartesian(1000, min_y=0.01)
+domain = domain.Axis(100000)
+#domain.transformation_matrix = domain.fixed_y_transformation
 window.plotter.add_graph('lyapunov', graph.Discrete2d(domain, SIMPLE))
 window.run()
