d29da5f39c7d9b2485feda6045897b3b0569fc24
Jesse Hinrichsen <Jesse Hinrichsen>
2015-07-06 09:30:02 +0200

    first equation for lyapunov

--- plot/lyapunov.py
+++ plot/lyapunov.py
@@ -5,7 +5,7 @@ OpenGL Kernel for lyapunov exponent
 """
 
 from plotting.app import PlotterWindow
-from plotting import graph, domain
+from plotting import graph, domain, widget
 
 SIMPLE = """
 float g(float r, float x) {
@@ -18,22 +18,64 @@ float h(float r, float x) {
     return v;
 }
 
-vec4 f(vec4 x) {
-    float y0 = 10;// = g(x.x, x.y);
-    float summe = y0;
-    float n = 1000;
-    for (int i = 1; i < n - 1; i+=1) {
-        y0 = h(x.x, y0);
-        summe += log(y0);
-    }
-    return vec4(x.x, summe/n, 0, 0.3);
+float g1(float r, float x) {
+    return r*sin(x);
 }
+
+float h1(float r, float x) {
+    return r*x*(1-x);
+}
+
+
+//vec4 fa(vec4 x) {
+//    float y0 = 10;// = g(x.x, x.y);
+//    float summe = y0;
+//    float n = 10000;
+//    for (int i = 1; i < n - 1; i+=1) {
+//        y0 = h(x.x, y0);
+//        if (i > 300) {
+//            summe += log(y0);
+//        }
+//    }
+//    return vec4(x.x, summe/n, 0, 0.3);
+//}
+
+uniform float eps;
+uniform float n;
+vec4 f(vec4 x) {
+    //float eps = 0.01;
+    float y = 0.8;
+    float y0 = y;
+    float y1 = y + eps;
+    //float n = 10;
+
+    float diff;
+    for (int i = 1; i < n; i+=1) {
+            //diff = abs(y1-y0);
+            //if (diff > eps) {
+            //    y1 = y0 + eps;
+            //}
+
+            y1 = g1(x.x, y1);
+            y0 = g1(x.x, y0);
+            
+        
+    }
+    float foo = log(abs((y1 - y0)/eps));
+
+    return vec4(x.x, foo/n, 0, 0.3);
+}
+
 """
 
 
 window = PlotterWindow(axis=(3.0,7.0), origin=(-1.0,5.0))
 #domain = domain.Cartesian(1000, min_y=0.01)
-domain = domain.Axis(100000)
+domain = domain.Axis(1000)
 #domain.transformation_matrix = domain.fixed_y_transformation
 window.plotter.add_graph('lyapunov', graph.Discrete2d(domain, SIMPLE))
+uniforms = window.plotter.get_uniform_manager()
+uniforms.set_global('eps', 0.01)
+uniforms.set_global('n', 1.0)
+window.add_widget('test', widget.Uniforms(uniforms))
 window.run()
